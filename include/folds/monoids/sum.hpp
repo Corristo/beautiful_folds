21b2c92f8d496241b694149981fe540983530087
Manuel Bergler <berglerma@gmail.com>
2020-01-02 14:26:08 +0100

    Remove always inline attribute

--- include/folds/monoids/sum.hpp
+++ include/folds/monoids/sum.hpp
@@ -6,21 +6,20 @@ namespace detail {
 
 template <typename T>
 struct SumMonoid {
-    T _value{};
+  T _value{};
 
-    friend SumMonoid<T> __attribute__((always_inline)) operator+(
-        SumMonoid<T> lhs, SumMonoid<T> rhs) {
-        return SumMonoid<T>{lhs._value + rhs._value};
-    }
+  friend SumMonoid<T> operator+(SumMonoid<T> lhs, SumMonoid<T> rhs) {
+    return SumMonoid<T>{lhs._value + rhs._value};
+  }
 };
 
 }  // namespace detail
 
 constexpr auto inline Sum = []<typename T>(T i) -> detail::SumMonoid<T> {
-    return detail::SumMonoid<T>{i};
+  return detail::SumMonoid<T>{i};
 };
 constexpr auto inline GetSum = []<typename T>(detail::SumMonoid<T> s) -> T {
-    return s._value;
+  return s._value;
 };
 
 }  // namespace folds
