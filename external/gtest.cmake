5b44c114cbe1b33231662769a33dac31b53f7c6c
Manuel Bergler <berglerma@gmail.com>
2020-04-02 17:12:16 +0200

    GTest: Work around bug #2457

--- external/gtest.cmake
+++ external/gtest.cmake
@@ -1,5 +1,5 @@
 FetchContent_Declare(
-  gtest
+  googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
   GIT_TAG master
 )
@@ -7,7 +7,7 @@ FetchContent_Declare(
 set(INSTALL_GTEST OFF)
 set(CMAKE_POLICY_DEFAULT_CMP0077 NEW)
 
-FetchContent_MakeAvailable(gtest)
+FetchContent_MakeAvailable(googletest)
 
 add_library(GTest::gtest ALIAS gtest)
 add_library(GTest::main ALIAS gtest_main)
